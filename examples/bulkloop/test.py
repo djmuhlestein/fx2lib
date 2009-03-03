6bca9c94dee103e153569ed1fc9f3da66ba3bb10
Dennis Muhlestein <dennis@ubixum.com>
2009-03-03 09:01:10 -0700

    Fixed forgotten import on python example.

--- examples/bulkloop/test.py
+++ examples/bulkloop/test.py
@@ -15,6 +15,9 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USAimport struct
 
+
+import struct 
+
 from fx2load import *
 
 openfx2(0x04b4,0x1004)
