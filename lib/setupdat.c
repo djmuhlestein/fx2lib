43e44f5b2a628e0d4c9626e9f29e1f332a7d577b
Dennis Muhlestein <dennis@ubixum.com>
2009-03-26 16:11:14 -0600

    don't printf in setupdat by default

--- lib/setupdat.c
+++ lib/setupdat.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  **/
 
-#define DEBUG_SETUPDAT
+//#define DEBUG_SETUPDAT
 
 #ifdef DEBUG_SETUPDAT
 #include <stdio.h> // NOTE this needs deleted
