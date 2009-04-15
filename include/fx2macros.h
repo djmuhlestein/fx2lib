c36df9f1f2e0e6481f3880d3e980bfbff4d52c4d
Dennis Muhlestein <dennis@ubixum.com>
2009-04-15 09:14:38 -0600

    Additional macro for an unconditional RENUMERATE as well as additional header documentation.

--- include/fx2macros.h
+++ include/fx2macros.h
@@ -34,17 +34,47 @@
 
 // clock stuff
 
+/**
+ * \brief Used for getting and setting the CPU clock speed.
+ **/
 typedef enum { CLK_12M =0, CLK_24M, CLK_48M} CLK_SPD;
+
+/**
+ * \brief Evaluates to a CLK_SPD enum.
+ **/
 #define CPUFREQ (CLK_SPD)((CPUCS & bmCLKSPD) >> 3)
+/**
+ * \brief Sets the cpu to operate at a specific
+ *  clock speed.
+ **/
 #define SETCPUFREQ(SPD) CPUCS = (CPUCS & ~bmCLKSPD) | (SPD << 3)
 
+/**
+ * \brief Evaluates to a DWORD value representing 
+ *  the clock speed in cycles per second. 
+ *
+ * Speeds:
+ *
+ *  \li 12000000L
+ *  \li 24000000L
+ *  \li 48000000L
+ *
+ **/
 #define XTAL (CPUFREQ==CLK_12M ? 12000000L :\
               CPUFREQ==CLK_24M ? 24000000L : 48000000L)
 
 
-// i2c stuff
+/**
+ * \brief Evaluates to the i2c bus frequency in cyles per
+ * second.
+ *
+ * Speeds:
+ *
+ * \li 400000L
+ * \li 100000L
+ *
+ **/
 #define I2CFREQ ((I2CTL & bm400KHZ) ? 400000L : 100000L)
-
 
 
 #define IFFREQ (IFCONFIG & bm3048MHZ ? 48000000L : 30000000L)
@@ -56,7 +86,20 @@ typedef enum { CLK_12M =0, CLK_24M, CLK_48M} CLK_SPD;
 #define EEPROM_TWO_BYTE (I2CS & bmBIT4)
 
 /**
- * \brief Cause the device to disconnect and reconnect to the USB bus.
+ * \brief Cause the device to disconnect and reconnect to USB.  This macro
+ *  unconditionally renumerates the device no matter how the firmware is loaded.
+ *
+ *  <b>NOTE</b> Windows really doesn't like this if the firmware is loaded
+ *  from an eeprom.  You'll see information messages about the device not
+ *  working properly.  On the other hand, if you are changing the device 
+ *  descriptor, e.g., the vendor and product id, when downloading firmware to the device, 
+ *  you'll need to use this macro instead of the  
+ *  standard RENUMERATE macro.
+ **/
+#define RENUMERATE_UNCOND() USBCS|=bmDISCON|bmRENUM;delay(1500);USBCS&=~bmDISCON
+/**
+ * \brief Cause the device to disconnect and reconnect to the USB bus.  This macro
+ *  doesn't do anything if the firmware is loaded from an eeprom.
  **/
 #define RENUMERATE() if(!(USBCS&bmRENUM)) {USBCS|=bmDISCON|bmRENUM;delay(1500);USBCS &= ~bmDISCON;}
 
@@ -64,12 +107,30 @@ typedef enum { CLK_12M =0, CLK_24M, CLK_48M} CLK_SPD;
 // interrupts
 // USB interrupts are in usbjt.h
 
+
+/**
+ * \brief TRUE if the FX2 has transitioned to high speed on the USB bus..
+ **/
+#define HISPEED (USBCS&bmHSM)
+
+
 #define RESUME_ISR 6
+
+/**
+ * \brief Enable the Resume Interrupt.  Requires EA=1 separately.
+ **/
 #define ENABLE_RESUME() ERESI = 1 
+
+/**
+ * \brief Clear the resume interrupt.  Use within the resume
+ *  interrupt handler.
+ **/
 #define CLEAR_RESUME() RESI=0;
 
 
-// did a remote wakeup occure
+/**
+ * \brief Evaluates to TRUE if a remote wakeup event has occurred.
+ **/
 #define REMOTE_WAKEUP() (((WAKEUPCS & bmWU) && (WAKEUPCS & bmWUEN)) || ((WAKEUPCS & bmWU2) && (WAKEUPCS & bmWU2EN)))
 
 #endif
